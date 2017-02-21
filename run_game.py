c2ab31b0f3dd365ed5a36e48fffb91f4e8cecfe1
Mike Tamillow <mikaeltamillow96@gmail.com>
2017-02-21 12:30:26 -0600

    run game screen

--- run_game.py
+++ run_game.py
@@ -1,3 +1,12 @@
-import The Evolution of Evil.__main__
+
+__author__ = "Mike Tamillow"
+
+"""This is an RPG where your main character slowly
+turns into a mass murder, just like most RPGs"""
+
+import the_evolution_of_evil.main
+
 if __name__ == "__main__":
-    The Evolution of Evil.__main__.main()
+    the_evolution_of_evil.main.main()
+    pg.quit()
+    sys.exit()
